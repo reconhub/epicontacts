652e618da7ea25b9d2c863d7f6a69ca81ccd38c6
Thibaut Jombart <thibautjombart@gmail.com>
2016-06-21 22:11:05 +0100

    added directionality; check passes with 1 warning

--- R/read_contacts.R
+++ R/read_contacts.R
@@ -12,15 +12,33 @@
 #'
 #' @param id an index or name indicating which column in \code{linelist} contains unique identifiers
 #'
+#' @param from an index or name indicating which column in \code{contacts} contains the first case of a contact
+#'
+#' @param to an index or name indicating which column in \code{contacts} contains the second case of a contact
+#'
+#' @param directed a logical indicating if contact are directed, or not; not that contacts will be
+#' indicated as 'from' and 'to' even in non directed contacts
+#'
 #' @return A list with two data.frames named 'linelist', and 'contacts'; the first column of
 #' 'linelist' is named 'id', and contains a vector of character corresponding to unique identifiers;
 #' the first two columns of 'contacts' are named 'from' and 'to', and indicate contacts.
 #'
 #' @examples
 #' ## make epi_contacts object from simulated Ebola data
-#' x <- read_contacts(ebola.sim$linelist, ebola.sim$contacts)
+#' x <- make_epi_contacts(ebola.sim$linelist, ebola.sim$contacts)
 #'
-make_epi_contacts <- function(linelist, contacts=NULL, id=1, from=1, to=2){
+#' ## test reordering of columns
+#' linelist <- ebola.sim$linelist[,rev(seq_len(ncol(ebola.sim$linelist)))]
+#' contacts <- ebola.sim$contacts[,rev(seq_len(ncol(ebola.sim$contacts)))]
+#' head(linelist)
+#' head(contacts)
+#'
+#' ## make object
+#' x <- make_epi_contacts(linelist, contacts, id="case.id", to="case.id", from="infector")
+#' head(x$linelist)
+#' head(x$contacts)
+#'
+make_epi_contacts <- function(linelist, contacts=NULL, id=1L, from=1L, to=2L, directed=FALSE){
     ## We read data from linelist, which needs to contain at least one case, and contacts, which are
     ## optional. Sanity checks will include standard class and dimensionality checks, as well as
     ## uniqueness of IDs in the line list, and enforcing 'character' type for the unique IDs, and
@@ -85,7 +103,7 @@ make_epi_contacts <- function(linelist, contacts=NULL, id=1, from=1, to=2){
     }
 
     ## Build final output
-    out <- list(linelist=linelist, contacts=contacts)
+    out <- list(linelist=linelist, contacts=contacts, directed=directed)
 
     class(out) <- c("epi_contacts", "list")
     return(out)
