d340f42c7dc5969c9ab08de46783ff81dc85a77f
Thibaut Jombart <thibautjombart@gmail.com>
2016-06-21 21:44:24 +0100

    renaming function and files

--- tests/testthat/test_read_contacts.R
+++ tests/testthat/test_read_contacts.R
@@ -1,13 +1,13 @@
 context("Converting data to epi_contact using read_contacts")
 
 test_that("Class and content are fine", {
-    x <- read_contacts(ebola.sim$linelist)
+    x <- make_epi_contacts(ebola.sim$linelist)
     expect_is(x, "epi_contacts")
     expect_is(x, "list")
     expect_is(x$linelist$id, "character")
 })
 
 test_that("Errors happen when they should", {
-    expect_error(read_contacts(ebola.sim$linelist[c(1,1,2,3,3,4,5,6),], ebola.sim$contacts),
+    expect_error(make_epi_contacts(ebola.sim$linelist[c(1,1,2,3,3,4,5,6),], ebola.sim$contacts),
                  "Duplicated IDs detected in the linelist")
 })
