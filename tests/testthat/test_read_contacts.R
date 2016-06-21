aeda93912df25f60137466a9d8f5fb683d526cb6
Thibaut Jombart <thibautjombart@gmail.com>
2016-06-21 21:30:12 +0100

    removed dep on outbreaks - dataset is in now

--- tests/testthat/test_read_contacts.R
+++ tests/testthat/test_read_contacts.R
@@ -1,17 +1,13 @@
 context("Converting data to epi_contact using read_contacts")
 
 test_that("Class and content are fine", {
-    if(require(outbreaks)){
-        x <- read_contacts(ebola.sim$linelist)
-        expect_is(x, "epi_contacts")
-        expect_is(x, "list")
-        expect_is(x$linelist$id, "character")
-    }
+    x <- read_contacts(ebola.sim$linelist)
+    expect_is(x, "epi_contacts")
+    expect_is(x, "list")
+    expect_is(x$linelist$id, "character")
 })
 
 test_that("Errors happen when they should", {
-    if(require(outbreaks)){
-        expect_error(read_contacts(ebola.sim$linelist[c(1,1,2,3,3,4,5,6),], ebola.sim$contacts),
-                     "Duplicated IDs detected in the linelist")
-    }
+    expect_error(read_contacts(ebola.sim$linelist[c(1,1,2,3,3,4,5,6),], ebola.sim$contacts),
+                 "Duplicated IDs detected in the linelist")
 })
