f520e17aa12bd0c36241af7ee858ba3abf9017b7
Finlay Campbell <f.campbell15@imperial.ac.uk>
2016-06-24 11:05:17 -0600

    added start and end date description

--- R/factor_subset.R
+++ R/factor_subset.R
@@ -22,42 +22,49 @@ factor_subset <- function(x,edge.attribute=NULL,node.attribute=NULL){
         warning("No factor provided, returning unmodified epi.contact object")
         return(x)
     }
-
+      
+    node.id <- x$linelist$id
+    edge.from <- x$contacts$from
+    edge.to <- x$contacts$to
+  
     if(!(is.null(node.attribute))){
 
         for(i in names(node.attribute)){
 
-            if(class(node.attribute[[i]]) %in% c("character","factor")){
-                x$linelist <- filter(x$linelist,x$linelist[[i]] %in% node.attribute[i])
+            if(class(node.attribute[[i]]) %in% c("character","factor","numeric")){
+                x$linelist <- dplyr::filter(x$linelist,x$linelist[[i]] %in% node.attribute[i])
             }
 
-            if(class(node.attribute[[i]]) %in% c("numeric","Date")){
-                x$linelist <- filter(x$linelist,x$linelist[[i]] < node.attribute[i])
+            if(class(node.attribute[[i]]) %in% c("Date")){
+              if(length(node.attribute[[i]])!=2) stop("Node attribute of class date must contain a start and end date")  
+              x$linelist <- dplyr::filter(x$linelist,x$linelist[[i]] > node.attribute[[i]][1] & x$linelist[[i]] < node.attribute[[i]][2])
             }
         }
 
         node.id <- x$linelist$id
+        
     }
-
 
     if(!(is.null(edge.attribute))){
 
         for(i in names(edge.attribute)){
 
             if(class(edge.attribute[[i]]) %in% c("character","factor")){
-                x$contacts <- filter(x$contacts,x$contacts[[i]] %in% edge.attribute[i])
+                x$contacts <- dplyr::filter(x$contacts,x$contacts[[i]] %in% edge.attribute[i])
             }
 
             if(class(edge.attribute[[i]]) %in% c("numeric","date")){
-                x$contacts <- filter(x$contacts,x$contacts[[i]] < edge.attribute[i])
+                x$contacts <- dplyr::filter(x$contacts,x$contacts[[i]] > edge.attribute[[i]][1] & x$contacts[[i]] < edge.attribute[[i]][2])
             }
         }
 
-        edge.id <- x$contacts[,1:2]
-    }
+        edge.from <- x$contacts$from
+        edge.to <- x$contacts$to
+        
+    } else
 
-    out <- x[node.id,edge.id$from,contacts="from"]
-    out <- out[node.id,edge.id$to,contacts="to"]
+    out <- x[i=node.id,j=edge.from,contacts="from"]
+    out <- out[i=node.id,edge.to,contacts="to"]
 
     return(out)
 }
