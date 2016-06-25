fefda0e98420986ca26e477d440147c4ed6d96b0
Thibaut Jombart <thibautjombart@gmail.com>
2016-06-25 08:23:40 +0100

    renamed 'node' to 'id'; also changed the function name accordingly; still need to rename file

--- R/subset_clusters_by_nodes.R
+++ R/subset_clusters_by_nodes.R
@@ -1,6 +1,7 @@
-#' Subset clusters from epi_contacts object by nodes
+#' Subset epi_contacts by case-specified clusters
 #'
-#' This function subsets an epi_contacts object based on individuals/nodes of interest.
+#' This function subsets an \code{\link{epi_contacts}} object by identifying clusters of cases
+#' connected to specified cases.
 #'
 #' @export
 #'
@@ -8,10 +9,11 @@
 #'
 #' @param epi_contacts an \code{\link{epi_contacts}} object
 #'
-#' @param nodes node(s) whose clusters need to be subsetted
+#' @param id a character vector of case identifiers; the connected components attached to these
+#' cases will be retained in the output object.
 #'
 #' @return An \code{\link{epi_contacts}} object whose contact dataframe corresponds to
-#' all clusters containing specified nodes.
+#' all clusters containing specified case id.
 #'
 #' @examples
 #' ## build data
@@ -20,21 +22,21 @@
 #'                        directed=TRUE)
 #'
 #'
-#' nodes <- "cac51e" ## it can be a vector of nodes as well
+#' id <- "cac51e" ## it can be a vector of id as well
 #'
 #'
 #' ## subset based on cluster to which "cac51e" belongs
-#' x_subset <- subset_clusters_by_nodes(x, nodes)
+#' x_subset <- subset_clusters_by_id(x, id)
 #'
 
 
-subset_clusters_by_nodes <- function(epi_contacts, nodes){
+subset_clusters_by_id <- function(epi_contacts, id){
     net <- igraph.epi_contacts(epi_contacts)
     cs <- igraph::clusters(net)
     net_nodes <- data.frame(nodes =igraph::V(net)$id, cs_member = cs$membership, stringsAsFactors = FALSE)
     cluster_to_subset <- unique(net_nodes$cs_member[which(net_nodes$nodes %in% nodes)])
-    nodes_to_subset <- net_nodes$nodes[ which(net_nodes$cs_member %in% cluster_to_subset)]
-    epi_subset <- epi_contacts[nodes_to_subset]
+    id_to_subset <- net_nodes$nodes[ which(net_nodes$cs_member %in% cluster_to_subset)]
+    epi_subset <- epi_contacts[id_to_subset]
     return(epi_subset)
 }
 
