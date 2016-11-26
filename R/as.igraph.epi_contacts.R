c91ca07e554ad5b7a274973df5c980e2258c37d1
nistara <nrandhawa@ucdavis.edu>
2016-11-26 10:31:48 +0300

    clean commented code (issue #51)

--- R/as.igraph.epi_contacts.R
+++ R/as.igraph.epi_contacts.R
@@ -31,37 +31,6 @@
 #' }
 
 as.igraph.epi_contacts <- function(x){
-#     ## We read in the epi_contacts object, which contains the two dataframes,
-#     ## the 'linelist' and the 'contacts' dataframes. This function converts
-#     ## the 'contacts' dataframe into an igraph object.  It extracts
-#     ## information for the igraph vertices from the corresponding 'linelist'
-#     ## dataframe (if they are present), and assigns them as vertx attributes.
-#
-#     ## extract contact data.frame
-#     net_edges <- x$contacts
-#
-#     ## create igraph object
-#     net <- igraph::graph.data.frame(net_edges, directed = x$directed)
-#
-#     ## adding vertex attributes
-#
-#     ### verts: vertices of igraph object
-#     verts <- data.frame(id = igraph::vertex_attr(net)[[1]], stringsAsFactors = FALSE)
-#
-#     ### attrs: the epi_contacts linelist, which serves as the source of the
-#     attributes attrs <- x$linelist
-#
-#     ### verts_info: attributes of vertices present in the linelist
-#     verts_info <- dplyr::left_join(verts, attrs, by = 'id')
-#
-#     ### attrs_list: the above attributes in a list format
-#     attrs_list <- lapply(verts_info, function(attr_col){attr_col})
-#
-#     ### assigning the attributes to the igraph igraph::vertex_attr(net) <-
-#     attrs_list # {{figure out if I should keep the "id" as an attr or not?}}
-
-    ## Take 2, simpler way to go about it:
-
     ## Create vertex dataframe using combination of linelist and contacts
     all_ids <- data.frame(id = get_id(x, "all"), stringsAsFactors = FALSE)
     verts <- dplyr::full_join(x$linelist, all_ids, by = "id")
