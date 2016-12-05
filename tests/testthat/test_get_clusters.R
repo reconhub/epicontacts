94a5fcd6e135a6448c0ab82cc7d49f49e005b0a9
VP Nagraj <vpnagraj@virginia.edu>
2016-12-05 11:50:50 -0500

    added another test for get_clusters

--- tests/testthat/test_get_clusters.R
+++ tests/testthat/test_get_clusters.R
@@ -12,3 +12,25 @@ test_that("igraph functions perform as expected", {
   expect_is(net, "igraph")
   
 })
+
+test_that("join of cluster id to linelist works", {
+  
+  skip_on_cran()
+  
+  x <- make_epi_contacts(ebola.sim$linelist, ebola.sim$contacts,
+    id="case.id", to="case.id", from="infector",
+    directed=TRUE)
+  
+  net <- as.igraph.epi_contacts(x)
+  cs <- igraph::clusters(net)
+  cs_size <- data.frame(cluster_member = seq_along(cs$csize),
+    cluster_size = cs$csize)
+  
+  net_nodes <- data.frame(id =igraph::V(net)$id,
+    cluster_member = cs$membership,
+    stringsAsFactors = FALSE)
+  
+  net_nodes <- dplyr::left_join(net_nodes, cs_size, by = "cluster_member")
+  expect_named(net_nodes, c("id", "cluster_member","cluster_size"), ignore.order = TRUE)
+  
+})
