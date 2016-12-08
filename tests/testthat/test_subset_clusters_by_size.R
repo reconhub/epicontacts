fa5c935fb63d803d1f223d5ec103e74a692ff4b2
Thibaut Jombart <thibautjombart@gmail.com>
2016-12-08 12:25:03 +0000

    formatting

--- tests/testthat/test_subset_clusters_by_size.R
+++ tests/testthat/test_subset_clusters_by_size.R
@@ -24,28 +24,30 @@ test_that("Returns clusters with the correct size", {
 
     skip_on_cran()
 
-    x <- make_epi_contacts(ebola.sim$linelist, ebola.sim$contacts, id="case.id",
-                           to="case.id", from="infector", directed=FALSE)
+    x <- make_epi_contacts(ebola.sim$linelist,
+                           ebola.sim$contacts, id="case.id",
+                           to="case.id", from="infector",
+                           directed=FALSE)
 
-    y <- subset_clusters_by_size(x,cs_max=10)
+    y <- subset_clusters_by_size(x, cs_max = 10)
     net <- as.igraph.epi_contacts(y)
     clusters <- igraph::clusters(net)
-    expect_true(all(clusters$csize<=10))
+    expect_true(all(clusters$csize <= 10))
 
     y <- subset_clusters_by_size(x,cs_min=5)
     net <- as.igraph.epi_contacts(y)
     clusters <- igraph::clusters(net)
-    expect_true(all(clusters$csize>=5))
+    expect_true(all(clusters$csize >= 5))
 
-    y <- subset_clusters_by_size(x,cs=5)
+    y <- subset_clusters_by_size(x, cs=5)
     net <- as.igraph.epi_contacts(y)
     clusters <- igraph::clusters(net)
-    expect_true(all(clusters$csize==5))
+    expect_true(all(clusters$csize == 5))
 
-    y <- subset_clusters_by_size(x,cs=5,cs_min=8,cs_max=15)
+    y <- subset_clusters_by_size(x, cs=5, cs_min=8, cs_max=15)
     net <- as.igraph.epi_contacts(y)
     clusters <- igraph::clusters(net)
-    expect_true(all(clusters$csize>=8))
-    expect_true(all(clusters$csize<=15))
+    expect_true(all(clusters$csize >= 8))
+    expect_true(all(clusters$csize <= 15))
 
 })
