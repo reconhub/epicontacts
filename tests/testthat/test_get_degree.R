5a2ceca249290844303bdffb269a048ae498d137
VP Nagraj <vpnagraj@virginia.edu>
2016-12-16 15:43:56 -0500

    added tests for get_degree

---
+++ tests/testthat/test_get_degree.R
@@ -0,0 +1,46 @@
+context("Computing number of contacts per case")
+
+test_that("the both argument is working as expected", {
+  
+  skip_on_cran()
+  
+  x <- make_epicontacts(ebola.sim$linelist, ebola.sim$contacts,
+    id="case.id", to="case.id", from="infector",
+    directed=TRUE)
+  
+  deg_both <- get_degree(x, "both")
+  deg_in <- get_degree(x, "in")
+  deg_out <- get_degree(x, "out")
+  
+  expect_equal((deg_in+deg_out),deg_both)
+  
+})
+
+test_that("get_degree is producing a named vector that is not null", {
+  
+  skip_on_cran()
+  
+  x <- make_epicontacts(ebola.sim$linelist, ebola.sim$contacts,
+    id="case.id", to="case.id", from="infector",
+    directed=TRUE)
+  
+  deg <- get_degree(x)
+  
+  expect_more_than(length(deg), 0)
+  expect_more_than(length(names(deg)), 0)
+  
+})
+
+test_that("degree computed only in line list is working as expected", {
+  
+  skip_on_cran()
+  
+  x <- make_epicontacts(ebola.sim$linelist, ebola.sim$contacts,
+    id="case.id", to="case.id", from="infector",
+    directed=TRUE)
+  
+  deg_in_ll <- get_degree(x, "in", only_linelist = T)
+  
+  expect_equal(names(deg_in_ll), x$linelist$id)
+  
+})
