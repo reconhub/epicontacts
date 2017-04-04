45d45bbc165fb1e11803c80944cbde9d4e7cadf5
thibautjombart <thibautjombart@gmail.com>
2017-04-04 17:52:29 +0100

    new version of the summary and its printing

--- R/print.summary_epicontacts.R
+++ R/print.summary_epicontacts.R
@@ -13,30 +13,53 @@
 #'
 print.summary_epicontacts <- function(x, ...){
 
-    if (!is.null(x$n_linelist)) {
-        cat("\n// number of records in linelist:", x$n_linelist)
+    cat("\n/// Overview //")
+    if (!is.null(x$n_id_linelist)) {
+        cat("\n  // number of unique IDs in linelist:", x$n_id_linelist)
     }
 
-    if (!is.null(x$n_unique_contacts)) {
-        cat("\n// number of unique records in contact list:", x$n_unique_contacts)
+    if (!is.null(x$n_id_contacts)) {
+        cat("\n  // number of unique IDs in contacts:", x$n_id_contacts)
     }
 
-    if (!is.null(x$n_common)) {
-        cat("\n// number of contacts appearing in linelist:", x$n_common)
+    if (!is.null(x$n_id_common)) {
+        cat("\n  // number of unique IDs in both:", x$n_id_common)
     }
 
-    if (!is.null(x$n_common)) {
-        cat("\n// percentage of contacts appearing in linelist:",
-            paste0(round(((x$n_common) / (x$n_linelist))*100,2),"%"))
+    if (!is.null(x$n_contacts)) {
+        cat("\n  // number of contacts:", x$n_contacts)
     }
 
+    if (!is.null(x$prop_contacts_in_linelist)) {
+        cat("\n  // contacts with both cases in linelist:",
+            round(100 * x$prop_contacts_in_linelist,3), "%")
+    }
+
+    cat("\n\n/// Degrees of the network //")
+    if (!is.null(x$deg_in)) {
+        cat("\n  // in-degree summary:\n")
+        print(x$deg_in)
+    }
+
+    if (!is.null(x$deg_out)) {
+        cat("\n  // out-degree summary:\n")
+        print(x$deg_out)
+    }
+
+    if (!is.null(x$deg_both)) {
+        cat("\n  // in and out degree summary:\n")
+        print(x$deg_both)
+    }
+
+    cat("\n/// Attributes //")
     if (!is.null(x$linelist_attributes)) {
-        cat("\n// attributes in linelist:", x$linelist_attributes)
+        cat("\n  // attributes in linelist:\n", x$linelist_attributes)
     }
 
     if (!is.null(x$contacts_attributes)) {
-        cat("\n// attributes in contacts:",x$contacts_attributes)
+        cat("\n\n  // attributes in contacts:\n", x$contacts_attributes)
     }
+
     cat("\n")
     
 }
