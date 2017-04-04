b75f4863abaf1b638af4b827130079327decfd5c
thibautjombart <thibautjombart@gmail.com>
2017-04-04 19:01:03 +0100

    fixed issue about long example runtime; now using dispatching for as.igraph

--- R/as.igraph.epicontacts.R
+++ R/as.igraph.epicontacts.R
@@ -1,7 +1,7 @@
 #' Create igraph object from contact data
 #'
-#' This function creates an igraph object from a given \code{\link{epicontacts}} object containing
-#' a 'contacts' dataframe.
+#' This function creates an igraph object from a given \code{\link{epicontacts}}
+#' object containing a 'contacts' dataframe.
 #'
 #' @export
 #'
@@ -11,23 +11,31 @@
 #'
 #' @return An \code{igraph} object (from the \code{igraph} package).
 #'
+#' @importFrom igraph as.igraph
+#' 
 #' @examples
 #'
-#' if (require(outbreaks)) {
+#' if (require(outbreaks) && require(igraph)) {
 #' ## build data
+#' 
 #' x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
 #'                        id="case.id", to="case.id", from="infector",
 #'                        directed=TRUE)
 #'
-#' ## subset data - keep 10 cases from linelist with contacts
-#' ids <- get_id(x, "common")[1:10]
+#' 
+#' ## subset data - keep 50 cases from linelist with contacts
+#' 
+#' ids <- get_id(x, "common")[1:50]
 #' ids
-#' x <- x[ids]
+#' x <- x[ids, ids]
 #'
+#' 
 #' ## make igraph object with associated attributes from epicontacts object
-#' net <- as.igraph.epicontacts(x)
+#' 
+#' net <- as.igraph(x)
 #' net
-#' plot(net)
+#' plot(net, vertex.label = "", vertex.size = 10,
+#'      vertex.color = cases_pal(50))
 #' }
 
 as.igraph.epicontacts <- function(x){
