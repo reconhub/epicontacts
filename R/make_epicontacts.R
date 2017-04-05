2818eec89d41800149a8f9cb3a39530550437dd4
thibautjombart <thibautjombart@gmail.com>
2017-04-05 16:21:50 +0100

    more tests

--- R/make_epicontacts.R
+++ R/make_epicontacts.R
@@ -78,8 +78,8 @@
 #' head(x$linelist)
 #' head(x$contacts)
 #' }
-make_epicontacts <- function(linelist, contacts, id=1L, from=1L, to=2L,
-                              directed=FALSE){
+make_epicontacts <- function(linelist, contacts, id = 1L, from = 1L, to = 2L,
+                             directed = FALSE){
     
     ## We read data from linelist, which needs to contain at least one case, and
     ## contacts. Sanity checks will include standard class
@@ -90,19 +90,21 @@ make_epicontacts <- function(linelist, contacts, id=1L, from=1L, to=2L,
 
     ## process linelist ##
     ## checks
-    linelist <- as.data.frame(linelist)
-
+    
     if (is.null(linelist)) {
         stop("linelist is NULL")
     }
-    if (length(linelist)==1 && is.na(linelist)) {
+
+    if (is.vector(linelist) &&
+        length(linelist) == 1L &&
+        is.na(linelist)) {
         stop("linelist is NA")
     }
+
+    linelist <- as.data.frame(linelist)
+
     if (nrow(linelist) < 1L) {
         stop("linelist should have at least one row")
-    }
-    if (ncol(linelist) < 1L) {
-        stop("linelist should have at least one column")
     }
 
     linelist[,id] <- as.character(linelist[,id])
@@ -110,14 +112,18 @@ make_epicontacts <- function(linelist, contacts, id=1L, from=1L, to=2L,
         msg <- paste(linelist[temp,id], collapse=" ")
         stop("Duplicated IDs detected in the linelist; culprits are: ", msg)
     }
+    
 
     ## reordering
+    
     if (is.character(id)) {
         id <- match(id, names(linelist))
     }
     names(linelist)[id] <- "id"
     linelist <- linelist[, c(id, setdiff(seq_len(ncol(linelist)), id))]
 
+
+    
     ## process contacts ##
     ## checks
     
@@ -125,27 +131,31 @@ make_epicontacts <- function(linelist, contacts, id=1L, from=1L, to=2L,
         stop("contacts is NULL")
     }
     
-    if (length(contacts)==1 && is.na(contacts)) {
+    if (is.vector(contacts) &&
+        length(contacts) == 1L &&
+        is.na(contacts)) {
         stop("contacts is NA")
     }
-    if (!is.null(contacts)){
-        if(nrow(contacts) < 1L) {
-            stop("contacts should have at least one row")
-        }
-        if (ncol(contacts) < 2L) {
-            stop("contacts should have at least two columns")
-        }
 
-        ## reordering
-        if (is.character(from)) {
-            from <- match(from, names(contacts))
-        }
-        if (is.character(to)) {
-            to <- match(to, names(contacts))
-        }
-        names(contacts)[c(from,to)] <- c("from","to")
-        contacts <- contacts[, c(from, to, setdiff(seq_len(ncol(contacts)), c(from,to)))]
+    contacts <- as.data.frame(contacts)
+    
+    if(nrow(contacts) < 1L) {
+        stop("contacts should have at least one row")
     }
+    if (ncol(contacts) < 2L) {
+        stop("contacts should have at least two columns")
+    }
+
+    ## reordering
+    if (is.character(from)) {
+        from <- match(from, names(contacts))
+    }
+    if (is.character(to)) {
+        to <- match(to, names(contacts))
+    }
+    names(contacts)[c(from,to)] <- c("from","to")
+    contacts <- contacts[, c(from, to, setdiff(seq_len(ncol(contacts)), c(from,to)))]
+    
 
     ## Build final output
     out <- list(linelist=linelist, contacts=contacts, directed=directed)
