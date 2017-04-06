9b54afa46ca6cc27c8d01ff8122699143d9161b5
Thibaut Jombart <thibautjombart@gmail.com>
2017-04-06 12:09:49 +0100

    adding new tests for get_clusters; changing the argument names for get_clusters

--- R/get_clusters.R
+++ R/get_clusters.R
@@ -1,51 +1,65 @@
 #' Assign cluster IDs to epicontacts data
 #'
-#' This function identifies transitive clusters (i.e. connected components) and adds this information to the linelist data.
+#' This function identifies transitive clusters (i.e. connected components) and
+#' adds this information to the linelist data.
 #'
 #' @export
 #'
 #' @author Nistara Randhawa (\email{nrandhawa@@ucdavis.edu})
 #'
-#' @param epicontacts an \code{\link{epicontacts}} object.
+#' @param x An \code{\link{epicontacts}} object.
 #'
-#' @param output either an \code{\link{epicontacts}} object or a \link{factor} containing cluster memberships to which members of \code{\link{epicontacts}} linelist belong to.
+#' @param output A character string indicating the type of output: either an
+#'   \code{\link{epicontacts}} object or a \link{data.frame} containing cluster
+#'   memberships to which members of \code{\link{epicontacts}} linelist belong
+#'   to.
 #'
-#' @return An \code{\link{epicontacts}} object whose 'linelist' dataframe contains new columns corresponding to cluster membership and size, or a \link{data.frame} containing member ids, cluster memberships as factors, and associated cluster sizes.
+#' @return An \code{\link{epicontacts}} object whose 'linelist' dataframe
+#'   contains new columns corresponding to cluster membership and size, or a
+#'   \link{data.frame} containing member ids, cluster memberships as factors,
+#'   and associated cluster sizes.
 #'
 #' @examples
 #' if (require(outbreaks)) {
 #' ## build data
+#'
 #' x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
 #'                        id="case.id", to="case.id", from="infector",
 #'                        directed=TRUE)
 #'
+#'
 #' ## add cluster membership and sizes to epicontacts 'linelist'
+#'
 #' y <- get_clusters(x, output = "epicontacts")
 #'
-#' ## return a data.frame with linelist member ids and cluster memberships as factors
+#'
+#' ## return a data.frame with linelist member ids and cluster memberships as
+#' ## factors
+#'
 #' z <- get_clusters(x, output = "factor")
+#'
 #' }
 
-get_clusters <- function(epicontacts, output = c("epicontacts", "factor")){
-    output <- match.arg(output)
-    net <- as.igraph.epicontacts(epicontacts)
-    cs <- igraph::clusters(net)
-    cs_size <- data.frame(cluster_member = seq_along(cs$csize),
-                          cluster_size = cs$csize)
-    
-    net_nodes <- data.frame(id =igraph::V(net)$id,
-                            cluster_member = cs$membership,
-                            stringsAsFactors = FALSE)
-    
-    net_nodes <- dplyr::left_join(net_nodes, cs_size, by = "cluster_member")
-    if(output == "epicontacts") {
-        epicontacts$linelist <- dplyr::left_join(epicontacts$linelist, net_nodes, by = "id")
-        return(epicontacts)
-    } else {
-        f <- net_nodes[ net_nodes$id %in% epicontacts$linelist$id, ]
-        f$cluster_member <- as.factor(f$cluster_member)
-        return(f)
-    }
+get_clusters <- function(x, output = c("epicontacts", "data.frame")){
+  output <- match.arg(output)
+  net <- as.igraph.epicontacts(x)
+  cs <- igraph::clusters(net)
+  cs_size <- data.frame(cluster_member = seq_along(cs$csize),
+                        cluster_size = cs$csize)
+
+  net_nodes <- data.frame(id =igraph::V(net)$id,
+                          cluster_member = cs$membership,
+                          stringsAsFactors = FALSE)
+
+  net_nodes <- dplyr::left_join(net_nodes, cs_size, by = "cluster_member")
+  if(output == "epicontacts") {
+    x$linelist <- dplyr::left_join(x$linelist, net_nodes, by = "id")
+    return(x)
+  } else {
+    f <- net_nodes[ net_nodes$id %in% x$linelist$id, ]
+    f$cluster_member <- as.factor(f$cluster_member)
+    return(f)
+  }
 }
 
 
--- tests/testthat/test_get_clusters.R
+++ tests/testthat/test_get_clusters.R
@@ -1,50 +1,95 @@
 context("Retrieving clusters from linelist with get_clusters")
 
 test_that("igraph functions perform as expected", {
-  
+
   skip_on_cran()
-  
-  x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
-    id="case.id", to="case.id", from="infector",
-    directed=TRUE)
-  
+
+  x <- make_epicontacts(ebola_sim$linelist,
+                        ebola_sim$contacts,
+                        id = "case.id",
+                        to = "case.id",
+                        from = "infector",
+                        directed=TRUE)
+
   net <- as.igraph.epicontacts(x)
+
   expect_is(net, "igraph")
-  
+
 })
 
+
+
+
+
+
 test_that("construction of net nodes works", {
-  
+
   skip_on_cran()
-  
-  x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
-    id="case.id", to="case.id", from="infector",
-    directed=TRUE)
-  
+
+  x <- make_epicontacts(ebola_sim$linelist,
+                        ebola_sim$contacts,
+                        id = "case.id",
+                        to = "case.id",
+                        from = "infector",
+                        directed = TRUE)
+
   net <- as.igraph.epicontacts(x)
   cs <- igraph::clusters(net)
   cs_size <- data.frame(cluster_member = seq_along(cs$csize),
     cluster_size = cs$csize)
-  
+
   net_nodes <- data.frame(id =igraph::V(net)$id,
     cluster_member = cs$membership,
     stringsAsFactors = FALSE)
-  
+
   net_nodes <- dplyr::left_join(net_nodes, cs_size, by = "cluster_member")
-  expect_named(net_nodes, c("id", "cluster_member","cluster_size"), ignore.order = TRUE)
-  
+
+  expect_named(net_nodes,
+               c("id", "cluster_member","cluster_size"),
+               ignore.order = TRUE)
+
 })
 
+
+
+
+
+
 test_that("get_clusters returns epicontacts object", {
-  
+
   skip_on_cran()
-  
-  x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
-    id="case.id", to="case.id", from="infector",
-    directed=TRUE)
-  
+
+  x <- make_epicontacts(ebola_sim$linelist,
+                        ebola_sim$contacts,
+                        id = "case.id",
+                        to = "case.id",
+                        from = "infector",
+                        directed = TRUE)
+
   y <- get_clusters(x)
-  
+
   expect_is(y, "epicontacts")
+})
+
+
+
+
+
+
+test_that("get_clusters returns data.frame", {
+
+  skip_on_cran()
+
+  x <- make_epicontacts(ebola_sim$linelist,
+                        ebola_sim$contacts,
+                        id = "case.id",
+                        to = "case.id",
+                        from = "infector",
+                        directed = TRUE)
+
+  y <- get_clusters(x, output = "data.frame")
+
+  expect_is(y, "data.frame")
+
 })
 
