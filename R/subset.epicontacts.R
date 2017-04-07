af859cdf0cb6a8e1a41d600eb17501d29d7c22f6
thibautjombart <thibautjombart@gmail.com>
2017-04-07 15:06:36 +0100

    tidying code of subset and adding tests for it

--- R/subset.epicontacts.R
+++ R/subset.epicontacts.R
@@ -1,4 +1,4 @@
-#' Subset an epi_contact object by factors
+#' Subset an epicontact object by factors
 #'
 #' This function subsets an \code{\link{epicontacts}} object based on node,
 #' edge and/or cluster attributes. Specifying node attributes will return an
@@ -16,13 +16,13 @@
 #'
 #' @param x an epi_contact object to be subsetted
 #'
-#' @param node.attribute a named list defining the node attribute name and node
+#' @param node_attribute a named list defining the node attribute name and node
 #'     attribute value (as a single value or vector of values).  Dates must be
 #'     provided as a vector of date objects, defining the range of dates
 #'     included in the subset. If only one date is provided, only node
 #'     attributes with that date will be returned.
 #'
-#' @param edge.attribute a named list defining the edge attribute name and edge
+#' @param edge_attribute a named list defining the edge attribute name and edge
 #'     attribute value (as a single value or vector of values).  Dates must be
 #'     provided as a vector of date objects, defining the range of dates
 #'     included in the subset. If only one date is provided, only edge
@@ -42,94 +42,112 @@
 #' @examples
 #' if (require(outbreaks)) {
 #' ## build data
+#'
 #' x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
-#' id="case.id", to="case.id", from="infector", directed=FALSE)
+#'                       id = "case.id", to = "case.id",
+#'                       from = "infector", directed = FALSE)
 #'
 #' ## subset based on node and edge attributes
-#' x_subset <- subset(x, node.attribute=list("gender"="f"),
-#'                    edge.attribute=list("source"="funeral"))
+#' x_subset <- subset(x, node_attribute = list("gender" = "f"),
+#'                    edge_attribute = list("source" = "funeral"))
 #'
+#' 
 #' ## subset a cluster connected to a given id
 #' ## (can be a vector of ids as well)
-#' id <- "cac51e"
-#' x_subset <- subset(x, cluster_id=id)
-#'
+#' ## here picking node with highest out-degree
+#' 
+#' id <- names(which.max(get_degree(x, "out")))
+#' x_subset <- thin(subset(x, cluster_id = id), 2)
+#' x_subset
+#' plot(x_subset)
+#' 
+#' 
 #' ## subset based on cluster size range
+#'
 #' x_subset <- subset(x, cs_min = 12, cs_max = 15)
 #'
+#'
 #' ## subset based on single cluster size
+#'
 #' x_subset <- subset(x, cs = 12)
 #'
+#'
 #' ## subset based on minimum cluster size
+#'
 #' x_subset <- subset(x, cs_min = 10)
 #'
+#'
 #' ## subset based on maximum cluster size
+#'
 #' x_subset <- subset(x, cs_max = 9)
+#'
+#' 
 #' }
 
-subset.epicontacts <- function(x,node.attribute = NULL, edge.attribute = NULL,
+subset.epicontacts <- function(x, node_attribute = NULL, edge_attribute = NULL,
                                 cluster_id = NULL, cs = NULL, cs_min = NULL,
                                 cs_max = NULL, ...){
-    ## A function to check node.attribute and edge.attribute for errors;
+    ## A function to check node_attribute and edge_attribute for errors;
     ## arguments are:
 
-    ## - name.attribute is the name of the attribute being checked
+    ## - name_attribute is the name of the attribute being checked
     ## (e.g. "date.of.infection")
 
-    ## - list.attributes is the named list of attributes (either node.attribute
-    ## or edge.attribute)
+    ## - list_attributes is the named list of attributes (either node_attribute
+    ## or edge_attribute)
 
     ## - dataset is the dataframe describing the attribute values in the data
     ## (x$linelist or x$contacts)
 
-    check <- function(name.attribute, list.attributes, dataset) {
+    check <- function(name_attribute, list_attributes, dataset) {
 
-        if (!name.attribute %in% names(dataset))
-            stop(paste(name.attribute,"is not an attribute found in dataset"))
+        if (!name_attribute %in% names(dataset))
+            stop(paste(name_attribute, "is not an attribute found in dataset"))
 
         ## Attribute is a single node or edge attribute value (e.g. "f", if
-        ## name.attribute is "gender")
-        attribute <- list.attributes[[name.attribute]]
+        ## name_attribute is "gender")
+        attribute <- list_attributes[[name_attribute]]
 
         ## Data is the set of node or attribute values found in the data (e.g. a
         ## vector of infection dates)
-        data <- dataset[[name.attribute]]
+        data <- dataset[[name_attribute]]
 
-        if (inherits(data,"Date")) {
-            if (!inherits(attribute,"Date"))
-                stop(paste(name.attribute,"must be provided as a date object"))
-            if (length(attribute)==1)
+        if (inherits(data, "Date")) {
+            if (!inherits(attribute, "Date"))
+                stop(paste(name_attribute, "must be provided as a date object"))
+            if (length(attribute) == 1)
                 attribute <- c(attribute,attribute)
             if (length(attribute)>2) {
                 attribute <- attribute[1:2]
                 warning(paste0("More than two date values provided for ",
-                               name.attribute,", using first two"))
+                               name_attribute, ", using first two"))
             }
-            if (attribute[1] > attribute[2]) {
-                attribute <- c(attribute[2], attribute[1])
-                warning(paste("First date provided for",
-                              name.attribute,"is greater than second,
-                               using second date as lower boundary"))
-            }
+
+            ## make sure dates are ordered, first = most ancient
+            
+            attribute <- sort(attribute)
+
             if (min(attribute) > max(data, na.rm=TRUE) ||
                 max(attribute) < min(data, na.rm=TRUE))
                 stop(paste("Dates provided for",
-                           name.attribute,
+                           name_attribute,
                            "fall outside the range of the dataset"))
 
         } else if (!attribute %in% data)
-            stop(paste("Value for", name.attribute, "is not found in dataset"))
+            stop(paste("Value for", name_attribute, "is not found in dataset"))
 
         return(attribute)
 
     }
+    
 
     ## A function to subset a dataset (x$linelist or x$contacts) by a node or
     ## edge attribute
-    find.id.to.keep <- function(name.attribute,list.attributes,dataset) {
+    
+    find_id_to_keep <- function(name_attribute,list_attributes,dataset) {
 
-        attribute <- list.attributes[[name.attribute]]
-        data <- dataset[[name.attribute]]
+        attribute <- list_attributes[[name_attribute]]
+        data <- dataset[[name_attribute]]
 
         if (inherits(attribute,"Date")) {
             out <- data >= attribute[1] & data <= attribute[2]
@@ -143,51 +161,53 @@ subset.epicontacts <- function(x,node.attribute = NULL, edge.attribute = NULL,
         return(out)
     }
 
-    ## Check if epicontacts object, node.attribute and edge.attribute are
+    
+    ## Check if epicontacts object, node_attribute and edge_attribute are
     ## provided correctly
+    
     if (!inherits(x, "epicontacts")) {
         stop("x is not an 'epicontacts' object")
     }
-    sub.attr <- c(edge.attribute, node.attribute,
+    sub_attr <- c(edge_attribute, node_attribute,
                   cluster_id, cs, cs_min, cs_max)
-    if (is.null(sub.attr)) {
+    if (is.null(sub_attr)) {
         warning("No subsetting attributes provided, returning input object")
         return(x)
     }
-    if (!is.null(node.attribute) &&
-        !inherits(node.attribute,"list")) {
-        stop("node.attribute is not a list")
+    if (!is.null(node_attribute) &&
+        !inherits(node_attribute,"list")) {
+        stop("node_attribute is not a list")
     }
-    if (!is.null(edge.attribute) &&
-        !inherits(edge.attribute,"list")) {
-        stop("edge.attribute is not a list")
+    if (!is.null(edge_attribute) &&
+        !inherits(edge_attribute,"list")) {
+        stop("edge_attribute is not a list")
     }
 
-    names.na <- names(node.attribute)
-    names.ea <- names(edge.attribute)
+    names_na <- names(node_attribute)
+    names_ea <- names(edge_attribute)
 
-    ## Apply checks to node.attribute and edge.attribute
-    node.attribute <- lapply(names.na, check, node.attribute, x$linelist)
-    names(node.attribute) <- names.na
+    ## Apply checks to node_attribute and edge_attribute
+    node_attribute <- lapply(names_na, check, node_attribute, x$linelist)
+    names(node_attribute) <- names_na
 
-    edge.attribute <- lapply(names.ea, check, edge.attribute, x$contacts)
-    names(edge.attribute) <- names.ea
+    edge_attribute <- lapply(names_ea, check, edge_attribute, x$contacts)
+    names(edge_attribute) <- names_ea
 
-    ## Apply the subs function across all attributes provided in node.attribute
-    if (!(is.null(node.attribute))) {
-        for (name.attribute in names.na) {
-            to.keep <- find.id.to.keep(name.attribute,
-                                       node.attribute,
+    ## Apply the subs function across all attributes provided in node_attribute
+    if (!(is.null(node_attribute))) {
+        for (name_attribute in names_na) {
+            to.keep <- find_id_to_keep(name_attribute,
+                                       node_attribute,
                                        x$linelist)
             x <- x[i = to.keep]
         }
     }
 
-    ## Apply the subs function across all attributes provided in edge.attribute
-    if (!(is.null(edge.attribute))){
-        for (name.attribute in names.ea) {
-            to.keep <- find.id.to.keep(name.attribute,
-                                       edge.attribute,
+    ## Apply the subs function across all attributes provided in edge_attribute
+    if (!(is.null(edge_attribute))){
+        for (name_attribute in names_ea) {
+            to.keep <- find_id_to_keep(name_attribute,
+                                       edge_attribute,
                                        x$contacts)
             x <- x[j = to.keep]
         }
--- tests/testthat/test_subset.epicontacts.R
+++ tests/testthat/test_subset.epicontacts.R
@@ -9,65 +9,67 @@ test_that("Return errors / warnings when expected", {
                           to = "case.id", from = "infector",
                           directed=FALSE)
 
-    not.epicontacts <- list("linelist" = ebola_sim$linelist,
+    not_epicontacts <- list("linelist" = ebola_sim$linelist,
                             "contacts" = ebola_sim$contacts,
                             "directed" = FALSE)
     expect_error(
-        subset.epicontacts(not.epicontacts,
-                           node.attribute = list("gender" = "f"),
-                           edge.attribute = list("source" = "funeral")),
+        subset.epicontacts(not_epicontacts,
+                           node_attribute = list("gender" = "f"),
+                           edge_attribute = list("source" = "funeral")),
         "x is not an 'epicontacts' object")
 
     expect_error(
         subset.epicontacts(x,
-                           node.attribute = list("gende" = "f"),
-                           edge.attribute = list("source" = "funeral")),
+                           node_attribute = list("gende" = "f"),
+                           edge_attribute = list("source" = "funeral")),
         "gende is not an attribute found in dataset")
 
     expect_error(
         subset.epicontacts(x,
-                           node.attribute = list("gender" = "f"),
-                           edge.attribute = list("sourc" = "funeral")),
+                           node_attribute = list("gender" = "f"),
+                           edge_attribute = list("sourc" = "funeral")),
         "sourc is not an attribute found in dataset")
 
     expect_error(
         subset.epicontacts(x,
-                           node.attribute = list("gender" = "n"),
-                           edge.attribute = list("source" = "funeral")),
+                           node_attribute = list("gender" = "n"),
+                           edge_attribute = list("source" = "funeral")),
         "Value for gender is not found in dataset")
 
     expect_error(
         subset.epicontacts(x,
-                           node.attribute = list("gender" = "f"),
-                           edge.attribute = list("source" = "funera")),
+                           node_attribute = list("gender" = "f"),
+                           edge_attribute = list("source" = "funera")),
         "Value for source is not found in dataset")
 
     expect_error(
         subset.epicontacts(x,
-                           node.attribute = c("gender"),
-                           edge.attribute = list("source" = "funeral")),
-        "node.attribute is not a list")
+                           node_attribute = c("gender"),
+                           edge_attribute = list("source" = "funeral")),
+        "node_attribute is not a list")
 
     expect_error(
         subset.epicontacts(x,
-                           node.attribute = list("gender"),
-                           edge.attribute = c("source" = "funeral")),
-        "edge.attribute is not a list")
+                           node_attribute = list("gender"),
+                           edge_attribute = c("source" = "funeral")),
+        "edge_attribute is not a list")
 
     expect_error(
         subset.epicontacts(
             x,
-            node.attribute = list("gender" = "f","date.of.infection" = c("2014-04-08","2015-03-28")),
-            edge.attribute = list("source" = "funeral")),
+            node_attribute =
+            list("gender" = "f",
+                 "date.of.infection" = c("2014-04-08", "2015-03-28")),
+            edge_attribute = list("source" = "funeral")),
         "date.of.infection must be provided as a date object")
     
     expect_warning(
         subset.epicontacts(
             x,
-            node.attribute = list("gender" = "f",
+            node_attribute = list("gender" = "f",
                 "date.of.infection" = 
                 as.Date(c("2014-04-08","2015-03-28","2014-04-08"))),
-            edge.attribute = list("source" = "funeral")),
+            edge_attribute = list("source" = "funeral")),
         "More than two date values provided for date.of.infection, using first two")
 
     expect_warning(
@@ -95,9 +97,9 @@ test_that("Returns epicontacts object subsetted correctly", {
 
     y <- subset.epicontacts(
         x,
-        node.attribute = list("gender" = "f",
+        node_attribute = list("gender" = "f",
             "date.of.infection" = dates),
-        edge.attribute = list("source" = "funeral"))
+        edge_attribute = list("source" = "funeral"))
 
     expect_is(y, "epicontacts")
     expect_true(
@@ -105,5 +107,16 @@ test_that("Returns epicontacts object subsetted correctly", {
         min(y$linelist$date.of.infection) >=  dates[1] &&
         max(y$linelist$date.of.infection) <= dates[2] &&
         all(y$contacts$source == "funeral"))
+
+
+    id <- names(which.max(get_degree(x, "out")))
+    z <- thin(subset(x, cluster_id = id), 2)
+    expect_equal_to_reference(z, file = "rds/z.rds")
+
+    
+    zz <- subset(x, cs = 10)
+    expect_equal_to_reference(zz, file = "rds/zz.rds")
+    expect_true(all(get_clusters(zz, "data.frame")$cluster_size == 10L))
+   
 
 })
