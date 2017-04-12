53128df17c52b9f8a11eac9b7306d89040c66600
Thibaut Jombart <thibautjombart@gmail.com>
2017-04-12 15:58:20 +0100

    cosmetic changes

--- R/as.igraph.epicontacts.R
+++ R/as.igraph.epicontacts.R
@@ -9,33 +9,33 @@
 #'
 #' @param x An \code{\link{epicontacts}} object.
 #'
-#' @return An \code{igraph} object (from the \code{igraph} package).
-#' Note: any column called "name" in the original linelist will be stored as a
-#' new vertex attribute in the \code{igraph} object named 'epicontacts_name'.
-#' This is due to the inherent behaviour of igraph creating its own 'name' vertex
-#' attribute.
+#' @return An \code{igraph} object (from the \code{igraph} package).  Note: any
+#'   column called "name" in the original linelist will be stored as a new
+#'   vertex attribute in the \code{igraph} object named 'epicontacts_name'.
+#'   This is due to the inherent behaviour of igraph creating its own 'name'
+#'   vertex attribute.
 #'
 #' @importFrom igraph as.igraph
-#' 
+#'
 #' @examples
 #'
 #' if (require(outbreaks) && require(igraph)) {
 #' ## build data
-#' 
-#' x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
-#'                        id="case.id", to="case.id", from="infector",
-#'                        directed=TRUE)
 #'
-#' 
+#' x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
+#'                       id = "case.id", to = "case.id", from ="infector",
+#'                       directed = TRUE)
+#'
+#'
 #' ## subset data - keep 50 cases from linelist with contacts
-#' 
+#'
 #' ids <- get_id(x, "common")[1:50]
 #' ids
 #' x <- x[ids, ids]
 #'
-#' 
+#'
 #' ## make igraph object with associated attributes from epicontacts object
-#' 
+#'
 #' net <- as.igraph(x)
 #' net
 #' plot(net, vertex.label = "", vertex.size = 10,
@@ -43,23 +43,29 @@
 #' }
 
 as.igraph.epicontacts <- function(x){
-    ## Create vertex dataframe using combination of linelist and contacts
-    all_ids <- data.frame(id = get_id(x, "all"), stringsAsFactors = FALSE)
-    verts <- dplyr::full_join(x$linelist, all_ids, by = "id")
 
-    ## Checking if a "name" column exists
-    if( "name" %in% colnames(verts)) {
-        verts$epicontacts_name = verts$name
-        verts$name = NULL
-    }
+  ## Create vertex dataframe using combination of linelist and contacts
 
-    ## Creating igraph object
-    net <- igraph::graph_from_data_frame(x$contacts, vertices = verts,
-                                 directed = x$directed)
+  all_ids <- data.frame(id = get_id(x, "all"), stringsAsFactors = FALSE)
+  verts <- dplyr::full_join(x$linelist, all_ids, by = "id")
 
-    igraph::vertex_attr(net)$id = igraph::vertex_attr(net)$name
 
-    return(net)
+  ## Checking if a "name" column exists
+
+  if ("name" %in% colnames(verts)) {
+    verts$epicontacts_name <- verts$name
+    verts$name <- NULL
+  }
+
+
+  ## Creating igraph object
+
+  net <- igraph::graph_from_data_frame(x$contacts, vertices = verts,
+                                       directed = x$directed)
+
+  igraph::vertex_attr(net)$id <- igraph::vertex_attr(net)$name
+
+  return(net)
 }
 
 
