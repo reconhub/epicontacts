b998ae67ca6fc39b52608d6dc367fc308850a2a6
Thibaut Jombart <thibautjombart@gmail.com>
2017-05-21 11:50:35 +0200

    externalising assert code

---
+++ R/internals.R
@@ -0,0 +1,75 @@
+
+## assert_xxx functions make basic tests on 'xxx' against the data ('x', an
+## epicontacts object), and return a possibly processed 'xxx'.
+
+assert_node_color <- function(x, node_color) {
+  if (length(node_color) > 1L) {
+    stop("'node_color' must indicate a single node attribute")
+  }
+  if (is.logical(node_color) && !node_color) {
+    node_color <- NULL
+  }
+  if (!is.null(node_color)) {
+    if (is.numeric(node_color)) {
+      node_color <- names(x$linelist)[node_color]
+    }
+
+    if (!node_color %in% names(x$linelist)) {
+      msg <- sprintf("node_color '%s' is not in the linelist", node_color)
+      stop(msg)
+    }
+  }
+
+  return(node_color)
+}
+
+
+
+
+
+
+assert_node_shape <- function(x, node_shape) {
+
+  if (length(node_shape) > 1L) {
+    stop("'node_shape' must indicate a single node attribute")
+  }
+  if (is.logical(node_shape) && !node_shape) {
+    node_shape <- NULL
+  }
+  if (!is.null(node_shape)) {
+    if (is.numeric(node_shape)) {
+      node_shape <- names(x$linelist)[node_shape]
+    }
+
+    if (!node_shape %in% names(x$linelist)) {
+      msg <- sprintf("node_shape '%s' is not in the linelist", node_shape)
+      stop(msg)
+    }
+  }
+
+  return(node_shape)
+}
+
+
+
+
+
+assert_annot <- function(x, annot) {
+  if (is.logical(annot) && sum(annot) == 0L) {
+    annot <- NULL
+  }
+  if (!is.null(annot)) {
+    if (is.numeric(annot) || is.logical(annot)) {
+      annot <- names(x$linelist)[annot]
+    }
+
+    if (!all(annot %in% names(x$linelist))) {
+      culprits <- annot[!annot %in% names(x$linelist)]
+      culprits <- paste(culprits, collapse = ", ")
+      msg <- sprintf("Annot '%s' is not in the linelist", culprits)
+      stop(msg)
+    }
+  }
+
+  return(annot)
+}
