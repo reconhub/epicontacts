ffa6e771688836350b634a5d9c587ec1d001bae5
Finlay Campbell <finlaycampbell93@gmail.com>
2021-10-14 15:37:12 +0200

    commented out obsolete tests

--- tests/testthat/test_plot.epicontacts.R
+++ tests/testthat/test_plot.epicontacts.R
@@ -1,34 +1,34 @@
 context("Plotting epicontacts")
 
-test_that("Plots as expected", {
+## test_that("Plots as expected", {
 
-  skip_on_cran()
+##   skip_on_cran()
 
-  x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
-                        id = "case_id",
-                        to = "case_id",
-                        from = "infector",
-                        directed=FALSE)
-  x <- thin(x[1:100], 2)
+##   x <- make_epicontacts(ebola_sim$linelist, ebola_sim$contacts,
+##                         id = "case_id",
+##                         to = "case_id",
+##                         from = "infector",
+##                         directed=FALSE)
+##   x <- thin(x[1:100], 2)
 
-  plot1 <- plot(x)
-  plot2 <- plot(x, thin = FALSE)
-  plot3 <- plot(x, "gender")
-  plot3bis <- plot(x, 8L)
-  plot4 <- plot(x, "hospital", editor = TRUE)
-  plot5 <- plot(x, "hospital", method = "graph3D")
-  plot6 <- plot(x, "hospital", method = "graph3D",
-                node_size = 3, edge_size = 2)
+##   plot1 <- plot(x)
+##   plot2 <- plot(x, thin = FALSE)
+##   plot3 <- plot(x, "gender")
+##   plot3bis <- plot(x, 8L)
+##   plot4 <- plot(x, "hospital", editor = TRUE)
+##   plot5 <- plot(x, "hospital", method = "graph3D")
+##   plot6 <- plot(x, "hospital", method = "graph3D",
+##                 node_size = 3, edge_size = 2)
 
-  ## Expect_equal_to_reference(plot1, file = "rds/plot1.rds")
-  ## expect_equal_to_reference(plot2, file = "rds/plot2.rds")
-  ## expect_equal_to_reference(plot3, file = "rds/plot3.rds")
-  ## expect_equal_to_reference(plot3bis, file = "rds/plot3.rds")
-  ## expect_equal_to_reference(plot4, file = "rds/plot4.rds")
-  ## expect_equal_to_reference(plot5, file = "rds/plot5.rds")
-  ## expect_equal_to_reference(plot6, file = "rds/plot6.rds")
+##   ## Expect_equal_to_reference(plot1, file = "rds/plot1.rds")
+##   ## expect_equal_to_reference(plot2, file = "rds/plot2.rds")
+##   ## expect_equal_to_reference(plot3, file = "rds/plot3.rds")
+##   ## expect_equal_to_reference(plot3bis, file = "rds/plot3.rds")
+##   ## expect_equal_to_reference(plot4, file = "rds/plot4.rds")
+##   ## expect_equal_to_reference(plot5, file = "rds/plot5.rds")
+##   ## expect_equal_to_reference(plot6, file = "rds/plot6.rds")
 
-})
+## })
 
 
 
